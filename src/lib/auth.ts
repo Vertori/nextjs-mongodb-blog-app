abca54b91ac40846ea545c005677a4b1903db68e
Vertori <115083330+Vertori@users.noreply.github.com>
2024-01-01 21:37:48 +0100

    add logged by github user to db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { connectWithDb } from "./utils";
+import { User } from "./models";
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -12,4 +15,29 @@ export const {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      console.log(user, account, profile);
+      if (account?.provider === "github") {
+        connectWithDb();
+        try {
+          const user = await User.findOne({ email: profile?.email });
+          // if there is no user, create a new one
+          if (!user) {
+            const newUser = new User({
+              username: profile?.login,
+              email: profile?.email,
+              image: profile?.avatar_url,
+            });
+            await newUser.save();
+          }
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
+      }
+      // if the user is already in db
+      return true;
+    },
+  },
 });
--- src/lib/models.ts
+++ src/lib/models.ts
@@ -35,8 +35,6 @@ const userSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      required: true,
-      min: 6,
     },
     img: {
       type: String,
